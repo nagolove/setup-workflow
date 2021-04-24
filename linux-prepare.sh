e0e6f87a634522cad1fac56eadff65d4d9202179
nagolove <dekar.own@gmail.com>
2021-04-24 11:03:36 +0300

    more.

--- linux-prepare.sh
+++ linux-prepare.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/bash
 
-pacman -S yay neovim base-devel cmake
+pacman -S yay neovim base-devel cmake oh-my-zsh
 
 mkdir myprojects
+cd myprojects
+git clone git@github.com:nagolove/engine.git
+
+cd scenes
+git clone git@github.com:nagolove/automato.git
+
+cd ~
+mkdir bin
+
+
